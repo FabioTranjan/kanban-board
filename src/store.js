69ad2dd0f4ef911e7665d552a142be437248abdb
fabio <fabio@ftr-c02v64.local>
2021-09-16 09:10:36 -0300

    Added axios and redux-thunk as dependencies.

--- src/store.js
+++ src/store.js
@@ -1,4 +1,9 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import tasksReducer from './reducers';
+import thunk from 'redux-thunk';
 
-export default createStore(tasksReducer);
+export default createStore(
+  tasksReducer,
+  applyMiddleware(thunk)
+  // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
